9cc2269801202e655407a3e9a366b6b6b8b1a943
kogan007 <coreykogan@gmail.com>
2021-02-22 18:38:04 -0500

    env vars

--- client.js
+++ client.js
@@ -5,7 +5,7 @@ import {ApolloClient, HttpLink, InMemoryCache} from '@apollo/client';
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
-    uri: `${process.env.BACKEND_URL}/graphql`,
+    uri: `${process.env.GATSBY_BACKEND_URL}/graphql`,
     fetch
   })
 });
