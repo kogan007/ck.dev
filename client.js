df2a0fc14dffbd439975fc5dd4114afaf78142bc
kogan007 <coreykogan@gmail.com>
2021-03-10 10:07:47 -0500

    persist apollo cache in storage

--- client.js
+++ client.js
@@ -1,9 +1,16 @@
 import fetch from 'isomorphic-fetch';
 import {ApolloClient, HttpLink, InMemoryCache} from '@apollo/client';
+import { persistCache, LocalStorageWrapper } from 'apollo3-cache-persist';
 
+const cache = new InMemoryCache();
+
+await persistCache({
+  cache,
+  storage: new LocalStorageWrapper(window.localStorage),
+});
 
 const client = new ApolloClient({
-  cache: new InMemoryCache(),
+  cache,
   link: new HttpLink({
     uri: `${process.env.GATSBY_BACKEND_URL}/graphql`,
     fetch
